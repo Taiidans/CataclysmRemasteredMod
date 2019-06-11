dfe52f39d221e6abb8ec8674dcb41841c347a0a2
Taiidans <43096388+Taiidans@users.noreply.github.com>
2019-06-11 17:23:48 +0100

    Add files via upload

---
+++ leveldata/campaign/beastwar/datfiles.lua
@@ -0,0 +1,7 @@
+Dictionaries =
+{
+    {
+        name = "locale:leveldata/campaign/beastwar/Mission06cata/Mission06Cata.dat",
+    },
+    
+}
---
+++ leveldata/campaign/beastwar/teamcolour.lua
@@ -0,0 +1,16 @@
+-- 0 = player
+-- 1 = Taiidan Republic
+-- 2 = Bentusi
+-- 3 = The Beast
+-- 4 = Taiidan Imperailsits 
+
+teamcolours = {
+	[0] = {{1, 1, 1},             {0, 0.502, 0.502},     "DATA:Badges/beast slayer.tga", {1, 1, 1},             "data:/effect/trails/tai_trail_clr.tga"}, 
+	[1] = {{0.365, 0.553, 0.667}, {0.8, 0.8, 0.8},       "DATA:Badges/taiidan-republic-badge.tga",               {0.365, 0.553, 0.667}, "data:/effect/trails/hgn_trail_clr.tga"},
+	[2] = {{1, 1, 1},             {0, 0.502, 0.502},     "DATA:Badges/bentusi.tga", {1, 1, 1},             "data:/effect/trails/tai_trail_clr.tga"},  
+	[3] = {{0.365, 0.553, 0.667}, {0.8, 0.8, 0.8},       "DATA:Badges/beast_red.tga",               {0.365, 0.553, 0.667}, "data:/effect/trails/hgn_trail_clr.tga"},
+        [4] = {{1,0.823529411764706,0},  {1, 0, 0},     "DATA:Badges/imperial taiidan.tga", {0.921,0.75,0.419},"data:/effect/trails/tai_trail_clr.tga"}, 
+}
+
+
+
